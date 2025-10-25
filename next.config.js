7d9fac4e4a3c8cb58bda803f3b59302cb0db79e4
AyGoub <ayoubgoubraim@gmail.com>
2025-10-25 16:56:17 +0200

    Fix CSS and JS loading issues - remove basePath configuration

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/AyGoub.github.io' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/AyGoub.github.io' : ''
+  }
 }
 
 module.exports = nextConfig
